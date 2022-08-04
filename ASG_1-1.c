400710331a069e5a0e5e8593bf04175632d9fae2
Ahmed Sayed Ali <mrahmedsayedali@gmail.com>
2022-08-04 15:09:08 +0200

    Update ASG_1-1.c

--- ASG_1-1.c
+++ ASG_1-1.c
@@ -12,9 +12,12 @@ int main() {
     int userNumber;
 
     printf("Enter The Number \n");
-    scanf("%d",&userNumber);
-
-
+    scanf("%d", &userNumber);
+//---------------------------------------------------------------->TEST'S
+    //printf("Test1 output : %d\n",((userNumber / 2) * 2));
+    //printf("Test2 output : %d\n",((userNumber & 1) == 0));
+//---------------------------------------------------------------->{{ CASE:1 }}
+/*
     if((userNumber & 1) == 0){
         printf("The number is odd = 0 \n");
         printf("The number is even = 1 \n");
@@ -22,6 +25,29 @@ int main() {
         printf("The number is odd = 1 \n");
         printf("The number is even = 0 \n");
     }
+*/
+//---------------------------------------------------------------->{{ CASE:2 }}
+/*
+    int output = (userNumber & 1);
+    switch (output) {
+        case 0: {
+            printf("The number is odd = 0 \n");
+            printf("The number is even = 1 \n");
+        }
+            break;
+        case 1: {
+            printf("The number is odd = 1 \n");
+            printf("The number is even = 0 \n");
+        }
+            break;
+        default:
+            printf("Yous Number %d \n", userNumber);
+    }
+*/
+//---------------------------------------------------------------->{{ CASE:3 }}
+    printf("The number is odd = %d \n", ((userNumber & 1) == 1));
+    printf("The number is even = %d \n", ((userNumber & 1) == 0));
+//---------------------------------------------------------------->
 
     return 0;
 }
