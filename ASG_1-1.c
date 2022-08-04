fd073e492a3e18663405968e0a31bf7f35db344e
Ahmed Sayed Ali <mrahmedsayedali@gmail.com>
2022-08-04 14:47:12 +0200

    ASG_1-1 😎😎😎😎😎

---
+++ ASG_1-1.c
@@ -0,0 +1,27 @@
+/*
+By : Ahmed Elsayed Abdulrahman Ali
+Email : mrahmedsayedali@gmail.com
+github : https://github.com/MrAhmedSayedAli
+*/
+
+#include <stdio.h>
+
+
+int main() {
+
+    int userNumber;
+
+    printf("Enter The Number \n");
+    scanf("%d",&userNumber);
+
+
+    if(userNumber %2 == 0){
+        printf("The number is odd = 0 \n");
+        printf("The number is even = 1 \n");
+    }else{
+        printf("The number is odd = 1 \n");
+        printf("The number is even = 0 \n");
+    }
+
+    return 0;
+}
